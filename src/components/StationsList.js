f4609fd4e88d10470ab86232ebf1d30f438e61ae
Janik <janik.kurtz2@googlemail.com>
2022-10-01 13:09:39 +0200

    Added Checking for stations

--- src/components/StationsList.js
+++ src/components/StationsList.js
@@ -5,8 +5,8 @@ import '../css/stationList.css';
 export default function StationsList({ stations }) {
   return (
     <div id="station-list">
-      {stations.length ? <small id='results'>{stations.length} Tankstellen gefunden</small> : ''}
-      {stations.length === 0 ? 'Bitte wählen Sie eine Stadt aus' : stations.map(station => <Station key={station.id} data={station} />)}
+      {stations && stations.length ? <small id='results'>{stations.length} Tankstellen gefunden</small> : ''}
+      {stations && stations.length === 0 ? 'Bitte wählen Sie eine Stadt aus' : stations.map(station => <Station key={station.id} data={station} />)}
     </div>
   )
 }
