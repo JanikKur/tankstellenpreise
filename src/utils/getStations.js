566d093c350c3c585c458eaba7c13e8464a995ef
Janik <janik.kurtz2@googlemail.com>
2022-10-01 13:08:43 +0200

    Hid Api Key

--- src/utils/getStations.js
+++ src/utils/getStations.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default async function getStations(lat, long, range){
-    return await axios.get(`https://creativecommons.tankerkoenig.de/json/list.php?lat=${lat}&lng=${long}&rad=${range}&sort=dist&type=all&apikey=f8ee6587-c721-d35a-5b5a-65ddbd4711a6`);
+    return await axios.get(`https://creativecommons.tankerkoenig.de/json/list.php?lat=${lat}&lng=${long}&rad=${range}&sort=dist&type=all&apikey=${process.env.REACT_APP_API_KEY}`);
 }
